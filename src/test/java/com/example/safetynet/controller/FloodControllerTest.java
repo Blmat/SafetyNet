0edd914f1cfd52c3b204e664a0fc9cae693de183
Mathieu Blanc <94637553+Blmat@users.noreply.github.com>
2022-07-05 19:17:53 +0200

    Resultat attendu au test FloodControllerTest

--- src/test/java/com/example/safetynet/controller/FloodControllerTest.java
+++ src/test/java/com/example/safetynet/controller/FloodControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -41,6 +42,7 @@ class FloodControllerTest {
                         .param("stations", "2"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().json("[{\"address\":\"eee\",\"flood\":[{\"firstName\":\"eee\",\"lastName\":\"eee\",\"age\":15,\"phone\":\"000\",\"medications\":null,\"allergies\":null}]}]"));
     }
     @Test
@@ -48,13 +50,16 @@ class FloodControllerTest {
         this.mvc.perform(MockMvcRequestBuilders.get("/flood/stations")
                         .param("a", "2"))
                 .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().is4xxClientError());
+                .andExpect(status().is4xxClientError())
+                .andExpect(status().reason("Required request parameter 'stations' for method parameter type String is not present"));
     }
     @Test
     public void getHouseholdByFireStationAddressTestWithEmptyParamValue() throws Exception {
         this.mvc.perform(MockMvcRequestBuilders.get("/flood/stations")
                         .param("stations", ""))
                 .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().is2xxSuccessful());
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("[]"));
     }
 }
