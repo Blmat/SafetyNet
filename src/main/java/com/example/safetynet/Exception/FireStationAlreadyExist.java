008d0f4f6c5cac5278703871014cf7c9b1a6d8e5
Mathieu Blanc <94637553+Blmat@users.noreply.github.com>
2022-07-05 23:25:11 +0200

    FireStaionAlreadyExist

--- src/main/java/com/example/safetynet/Exception/FireStationAlreadyExist.java
+++ src/main/java/com/example/safetynet/Exception/FireStationAlreadyExist.java
@@ -3,6 +3,6 @@ package com.example.safetynet.Exception;
 public class FireStationAlreadyExist extends RuntimeException {
 
     public FireStationAlreadyExist(String message) {
-        super(message);
+        super("The Firestation already exists" );
     }
 }
