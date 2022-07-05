73733261f9ad2374c44aeacc4a584e5d89817daf
Mathieu Blanc <94637553+Blmat@users.noreply.github.com>
2022-07-05 19:14:28 +0200

    Resultat attendu au test FireStationCoveredControllerTest.java

--- src/test/java/com/example/safetynet/controller/FireStationCoveredControllerTest.java
+++ src/test/java/com/example/safetynet/controller/FireStationCoveredControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(FireStationCoveredController.class)
@@ -37,7 +39,9 @@ class FireStationCoveredControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/fire")
                         .param("address", "1509 Culver St"))
                 .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().is2xxSuccessful());
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("[{\"firstName\":\"John\",\"lastName\":\"Boyd\",\"age\":38,\"phone\":\"841-874-6512\",\"medications\":null,\"allergies\":null,\"stationNumber\":null}]"));
     }
 
     @Test
@@ -46,6 +50,7 @@ class FireStationCoveredControllerTest {
                         .param("a", "1509 Culver St"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().is4xxClientError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().reason("Required request parameter 'address' for method parameter type String is not present"));
     }
 
@@ -54,6 +59,10 @@ class FireStationCoveredControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/fire")
                         .param("address", "8 rue de la paix"))
                 .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().is2xxSuccessful());
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("[]"));
+
+
     }
 }
